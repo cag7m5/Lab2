e111e6d03c39199a771cdc8840a874dac1da53aa
MWalden13 <mkwty8@umsystem.edu>
2020-09-23 17:15:15 -0500

    Getting buffer overflow

--- assembler_mips.c
+++ assembler_mips.c
@@ -8,7 +8,6 @@ int main(int argc, char *argv[]) {
 
 	FILE *fpi;	//input file
 	FILE *fpo;	//output file
-	int i = 1;
 	
 	if (argc != 2){
 		printf("\nERROR: Correct Sequence: ./assembler_mips <input file>\n");
@@ -23,33 +22,32 @@ int main(int argc, char *argv[]) {
 	}
 	
 	char line[100];
-	char instruct[10];	//Store instruction name
+	char instruct[10] = "";	//Store instruction name
 	char R1[2];	//Store first register
 	char R2[2];	//Store second register
 	char R3[2];	//Store third register
-	int i = 0;	//Used to parse line
 	
 	while(fgets(line, 100, fpi) != NULL){		//Reads through file line by line
-		printf("Line %d is: %s\n", i, line);	//Debug
-		i++;
-		
-		instruct = strtok(line, " ");	//Breaks line into instruction name by looking for space char
+		printf("Line is: %s\n", line);	//Debug
+		int i = 0;	//Used to parse line
+		//instruct = strtok(line, " ");	//Breaks line into instruction name by looking for space char
+		strncpy(instruct, strtok(line, " "), (sizeof instruct) - 1);
 		printf("Instruction: %s\n", instruct);	//Debug
 		
 		if(strcmp(instruct, "addiu") == 0){
 			while(line != NULL){
 				if(i == 0){
-					R1 = strtok(line, " $(),");
+					strcpy(R1, strtok(line, " $(),"));
 					printf("%s", R1);
-					i++
+					i++;
 				}
 				else if(i == 2){
-					R2 = strtok(line, " $(),");
+					strcpy(R2, strtok(line, " $(),"));
 					printf("%s", R2);
 					i++;
 				}
 				else if(i == 3){
-					R3 = strtok(line, " $(),");
+					strcpy(R3, strtok(line, " $(),"));
 					printf("%s", R3);
 					i++;
 				}
