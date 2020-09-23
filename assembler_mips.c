a460fae50f61314f8b98f2b7a47fface5e0cf798
MWalden13 <70349366+MWalden13@users.noreply.github.com>
2020-09-23 16:02:33 -0500

    Working on parsing single instruction

--- assembler_mips.c
+++ assembler_mips.c
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[]) {
 
 	FILE *fpi;	//input file
-	FILE *fpo	//output file
+	FILE *fpo;	//output file
 	int i = 1;
 	
 	if (argc != 2){
@@ -23,11 +23,41 @@ int main(int argc, char *argv[]) {
 	}
 	
 	char line[100];
-	while(fgets(line, 100, fpi) != NULL){
-		printf("Line %d is: %s\n", i, line);
+	char instruct[10];	//Store instruction name
+	char R1[2];	//Store first register
+	char R2[2];	//Store second register
+	char R3[2];	//Store third register
+	int i = 0;	//Used to parse line
+	
+	while(fgets(line, 100, fpi) != NULL){		//Reads through file line by line
+		printf("Line %d is: %s\n", i, line);	//Debug
 		i++;
+		
+		instruct = strtok(line, " ");	//Breaks line into instruction name by looking for space char
+		printf("Instruction: %s\n", instruct);	//Debug
+		
+		if(strcmp(instruct, "addiu") == 0){
+			while(line != NULL){
+				if(i == 0){
+					R1 = strtok(line, " $(),");
+					printf("%s", R1);
+					i++
+				}
+				else if(i == 2){
+					R2 = strtok(line, " $(),");
+					printf("%s", R2);
+					i++;
+				}
+				else if(i == 3){
+					R3 = strtok(line, " $(),");
+					printf("%s", R3);
+					i++;
+				}
+			}
+		}
 	}
 	
+	fclose(fpi);
 
 	return 0;
 
