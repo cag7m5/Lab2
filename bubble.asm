05093d9f01560027b9e97b4ef0acd5ee5270cbb5
MWalden13 <70349366+MWalden13@users.noreply.github.com>
2020-09-29 14:55:08 -0500

    Create bubble.asm

---
+++ bubble.asm
@@ -0,0 +1,20 @@
+addiu $t0, $zero, 5
+addiu $t1, $zero, 3
+addiu $t2, $zero, 6
+addiu $t3, $zero, 8
+addiu $t4, $zero, 9
+addiu $t5, $zero, 1
+addiu $t6, $zero, 4
+addiu $t7, $zero, 7
+addiu $t8, $zero, 2
+addiu $t9, $zero, A
+sw $t0, 0($gp)
+sw $t1, 4($gp)
+sw $t2, 8($gp)
+sw $t3, C($gp)
+sw $t4, 10($gp)
+sw $t5, 14($gp)
+sw $t6, 18($gp)
+sw $t7, 1C($gp)
+sw $t8, 20($gp)
+sw $t9, 24($gp)
