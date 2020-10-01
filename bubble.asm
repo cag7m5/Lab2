8723df15d18c8f7c15a608c9455dccb0424b7a67
Caleb Gilbert <cag7m5@mail.missouri.edu>
2020-10-01 12:18:06 -0500

    bubble sort finishes now

--- bubble.asm
+++ bubble.asm
@@ -19,40 +19,40 @@ sw $t6, 18($a0)
 sw $t7, 1c($a0)
 sw $t8, 20($a0)
 sw $t9, 24($a0) 
-sub $s0, $t0, $t1
-bgtz $s0, 5c 
+sub $s0, $t1, $t0
+bgtz $s0, 3 
 sw $t1, 0($a0)
 sw $t0, 4($a0) 
-sub $s0, $t1, $t2
-bgtz $s0, 6c 
+sub $s0, $t2, $t1
+bgtz $s0, 3 
 sw $t2, 4($a0) 
 sw $t1, 8($a0) 
-sub $s0, $t2, $t3
-bgtz $s0, 7c 
+sub $s0, $t3, $t2
+bgtz $s0, 3 
 sw $t3, 8($a0)
 sw $t2, c($a0)
-sub $s0, $t3, $t4
-bgtz $s0, 8c 
+sub $s0, $t4, $t3
+bgtz $s0, 3 
 sw $t4, c($a0)
 sw $t3, 10($a0)
-sub $s0, $t4, $t5
-bgtz $s0, 9c 
+sub $s0, $t5, $t4
+bgtz $s0, 3 
 sw $t5, 10($a0)
 sw $t4, 14($a0)
-sub $s0, $t5, $t6
-bgtz $s0, 10c 
+sub $s0, $t6, $t5
+bgtz $s0, 3 
 sw $t6, 14($a0)
 sw $t5, 18($a0)
-sub $s0, $t6, $t7
-bgtz $s0, 11c 
+sub $s0, $t7, $t6
+bgtz $s0, 3 
 sw $t7, 18($a0)
 sw $t6, 1c($a0)
-sub $s0, $t7, $t8
-bgtz $s0, 12c 
+sub $s0, $t8, $t7
+bgtz $s0, 3 
 sw $t8, 1c($a0)
 sw $t7, 20($a0)
-sub $s0, $t8, $t9
-bgtz $s0, 13c 
+sub $s0, $t9, $t8
+bgtz $s0, 3 
 sw $t9, 20($a0)
 sw $t8, 24($a0)
 addiu $v0, $zero, 0xa
