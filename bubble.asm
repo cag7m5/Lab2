9998535d60b8dc8f68de9971870d18fee7a46866
MWalden13 <70349366+MWalden13@users.noreply.github.com>
2020-09-30 22:29:35 -0500

    Took out line numbers

--- bubble.asm
+++ bubble.asm
@@ -2,31 +2,31 @@ lui $a0, 0x1001
 addiu $t0, $zero, 5 
 addiu $t1, $zero, 3 
 addiu $t2, $zero, 6
-addiu $t3, $zero, 8 --10
+addiu $t3, $zero, 8 
 addiu $t4, $zero, 9
 addiu $t5, $zero, 1 
 addiu $t6, $zero, 4
-addiu $t7, $zero, 7 --20
+addiu $t7, $zero, 7 
 addiu $t8, $zero, 2
 addiu $t9, $zero, a 
 sw $t0, 0($a0)
-sw $t1, 4($a0) --30
+sw $t1, 4($a0) 
 sw $t2, 8($a0)
 sw $t3, c($a0)  
 sw $t4, 10($a0)
-sw $t5, 14($a0) --40
+sw $t5, 14($a0) 
 sw $t6, 18($a0)
 sw $t7, 1c($a0)
 sw $t8, 20($a0)
-sw $t9, 24($a0) --50
+sw $t9, 24($a0) 
 sub $s0, $t0, $t1
 bgtz $s0, 5c 
 sw $t1, 0($a0)
-sw $t0, 4($a0) --60
+sw $t0, 4($a0) 
 sub $s0, $t1, $t2
 bgtz $s0, 6c 
 sw $t2, 4($a0) 
-sw $t1, 8($a0) --70
+sw $t1, 8($a0) 
 sub $s0, $t2, $t3
 bgtz $s0, 7c 
 sw $t3, 8($a0)
