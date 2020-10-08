4768e98def88426c614c91080d668438f31b83ec
MWalden13 <70349366+MWalden13@users.noreply.github.com>
2020-10-08 15:12:20 -0500

    Update bubble.asm

--- bubble.asm
+++ bubble.asm
@@ -19,42 +19,276 @@ sw $t6, 18($a0)
 sw $t7, 1c($a0)
 sw $t8, 20($a0)
 sw $t9, 24($a0) 
+main:
 sub $s0, $t1, $t0
-bgtz $s0, 3 
-sw $t1, 0($a0)
-sw $t0, 4($a0) 
+bgtz $s0, swap1
+  swap1:
+  sw $t1, 0($a0)
+  sw $t0, 4($a0)
+  j main
+sub $s0, $t2, $t0
+bgtz $s0, swap2
+  swap2:
+  sw $t2, 0($a0)
+  sw $t0, 8($a0)
+  j main
+sub $s0, $t3, $t0
+bgtz $s0, swap3
+  swap3:
+  sw $t3, 0($a0)
+  sw $t0, c($a0)
+  j main
+sub $s0, $t4, $t0
+bgtz $s0, swap4
+  swap4:
+  sw $t4, 0($a0)
+  sw $t0, 10($a0)
+  j main
+sub $s0, $t5, $t0
+bgtz $s0, swap5
+  swap5:
+  sw $t5, 0($a0)
+  sw $t0, 14($a0)
+  j main
+sub $s0, $t6, $t0
+bgtz $s0, swap6
+  swap6:
+  sw $t6, 0($a0)
+  sw $t0, 18($a0)
+  j main
+sub $s0, $t7, $t0
+bgtz $s0, swap7
+  swap7:
+  sw $t7, 0($a0)
+  sw $t0, 1c($a0)
+  j main
+sub $s0, $t8, $t0
+bgtz $s0, swap8
+  swap8:
+  sw $t8, 0($a0)
+  sw $t0, 20($a0)
+  j main
+sub $s0, $t9, $t0
+bgtz $s0, swap9
+  swap9:
+  sw $t9, 0($a0)
+  sw $t0, 24($a0)
+  j main
 sub $s0, $t2, $t1
-bgtz $s0, 3 
-sw $t2, 4($a0) 
-sw $t1, 8($a0) 
+bgtz $s0, swap10
+  swap10:
+  sw $t2, 4($a0) 
+  sw $t1, 8($a0) 
+  j main
+sub $s0, $t3, $t1
+bgtz $s0, swap11
+  swap11:
+  sw $t3, 4($a0) 
+  sw $t1, c($a0) 
+  j main
+sub $s0, $t4, $t1
+bgtz $s0, swap12
+  swap12:
+  sw $t4, 4($a0) 
+  sw $t1, 10($a0) 
+  j main
+sub $s0, $t5, $t1
+bgtz $s0, swap13
+  swap13:
+  sw $t5, 4($a0) 
+  sw $t1, 14($a0) 
+  j main
+sub $s0, $t6, $t1
+bgtz $s0, swap14
+  swap14:
+  sw $t6, 4($a0) 
+  sw $t1, 18($a0) 
+  j main
+sub $s0, $t7, $t1
+bgtz $s0, swap15
+  swap15:
+  sw $t7, 4($a0) 
+  sw $t1, 1c($a0) 
+  j main
+sub $s0, $t8, $t1
+bgtz $s0, swap16
+  swap16:
+  sw $t8, 4($a0) 
+  sw $t1, 20($a0) 
+  j main
+sub $s0, $t9, $t1
+bgtz $s0, swap17
+  swap17:
+  sw $t9, 4($a0) 
+  sw $t1, 24($a0) 
+  j main
 sub $s0, $t3, $t2
-bgtz $s0, 3 
-sw $t3, 8($a0)
-sw $t2, c($a0)
+bgtz $s0, swap18
+  swap18:
+  sw $t3, 8($a0)
+  sw $t2, c($a0)
+  j main
+sub $s0, $t4, $t2
+bgtz $s0, swap19
+  swap19:
+  sw $t4, 8($a0)
+  sw $t2, 10($a0)
+  j main
+sub $s0, $t5, $t2
+bgtz $s0, swap20
+  swap20:
+  sw $t5, 8($a0)
+  sw $t2, 14($a0)
+  j main
+sub $s0, $t6, $t2
+bgtz $s0, swap21
+  swap21:
+  sw $t6, 8($a0)
+  sw $t2, 18($a0)
+  j main
+sub $s0, $t7, $t2
+bgtz $s0, swap22
+  swap22:
+  sw $t7, 8($a0)
+  sw $t2, 1c($a0)
+  j main
+sub $s0, $t8, $t2
+bgtz $s0, swap23
+  swap23:
+  sw $t8, 8($a0)
+  sw $t2, 20($a0)
+  j main
+sub $s0, $t9, $t2
+bgtz $s0, swap24
+  swap24:
+  sw $t9, 8($a0)
+  sw $t2, 24($a0)
+  j main
 sub $s0, $t4, $t3
-bgtz $s0, 3 
-sw $t4, c($a0)
-sw $t3, 10($a0)
+bgtz $s0, swap25
+  swap25:
+  sw $t4, c($a0)
+  sw $t3, 10($a0)
+  j main
+sub $s0, $t5, $t3
+bgtz $s0, swap26
+  swap26:
+  sw $t5, c($a0)
+  sw $t3, 14($a0)
+  j main
+sub $s0, $t6, $t3
+bgtz $s0, swap27
+  swap27:
+  sw $t6, c($a0)
+  sw $t3, 18($a0)
+  j main
+sub $s0, $t7, $t3
+bgtz $s0, swap28
+  swap28:
+  sw $t7, c($a0)
+  sw $t3, 1c($a0)
+  j main
+sub $s0, $t8, $t3
+bgtz $s0, swap29
+  swap29:
+  sw $t8, c($a0)
+  sw $t3, 20($a0)
+  j main
+sub $s0, $t9, $t3
+bgtz $s0, swap30
+  swap30:
+  sw $t4, c($a0)
+  sw $t3, 24($a0)
+  j main
 sub $s0, $t5, $t4
-bgtz $s0, 3 
-sw $t5, 10($a0)
-sw $t4, 14($a0)
+bgtz $s0, swap31
+  swap31:
+  sw $t5, 10($a0)
+  sw $t4, 14($a0)
+  j main
+sub $s0, $t6, $t4
+bgtz $s0, swap32
+  swap32:
+  sw $t6, 10($a0)
+  sw $t4, 18($a0)
+  j main
+sub $s0, $t7, $t4
+bgtz $s0, swap33
+  swap33:
+  sw $t7, 10($a0)
+  sw $t4, 1c($a0)
+  j main
+sub $s0, $t8, $t4
+bgtz $s0, swap34
+  swap34:
+  sw $t8, 10($a0)
+  sw $t4, 20($a0)
+  j main
+sub $s0, $t9, $t4
+bgtz $s0, swap35
+  swap35:
+  sw $t9, 10($a0)
+  sw $t4, 24($a0)
+  j main
 sub $s0, $t6, $t5
-bgtz $s0, 3 
-sw $t6, 14($a0)
-sw $t5, 18($a0)
+bgtz $s0, swap36
+  swap36:
+  sw $t6, 14($a0)
+  sw $t5, 18($a0)
+  j main
+sub $s0, $t7, $t5
+bgtz $s0, swap37
+  swap37:
+  sw $t7, 14($a0)
+  sw $t5, 1c($a0)
+  j main
+sub $s0, $t8, $t5
+bgtz $s0, swap38
+  swap38:
+  sw $t8, 14($a0)
+  sw $t5, 20($a0)
+  j main
+sub $s0, $t9, $t5
+bgtz $s0, swap39
+  swap39:
+  sw $t9, 14($a0)
+  sw $t5, 24($a0)
+  j main
 sub $s0, $t7, $t6
-bgtz $s0, 3 
-sw $t7, 18($a0)
-sw $t6, 1c($a0)
+bgtz $s0, swap40
+  swap40:
+  sw $t7, 18($a0)
+  sw $t6, 1c($a0)
+  j main
+sub $s0, $t8, $t6
+bgtz $s0, swap41
+  swap41:
+  sw $t8, 18($a0)
+  sw $t6, 20($a0)
+  j main
+sub $s0, $t9, $t6
+bgtz $s0, swap42
+  swap42:
+  sw $t9, 18($a0)
+  sw $t6, 24($a0)
+  j main
 sub $s0, $t8, $t7
-bgtz $s0, 3 
-sw $t8, 1c($a0)
-sw $t7, 20($a0)
+bgtz $s0, swap43
+  swap43:
+  sw $t8, 1c($a0)
+  sw $t7, 20($a0)
+  j main
+sub $s0, $t9, $t7
+bgtz $s0, swap44
+  swap44:
+  sw $t9, 1c($a0)
+  sw $t7, 24($a0)
+  j main
 sub $s0, $t9, $t8
-bgtz $s0, 3 
-sw $t9, 20($a0)
-sw $t8, 24($a0)
+bgtz $s0, swap45
+  swap45:
+  sw $t9, 20($a0)
+  sw $t8, 24($a0)
+  j main
 addiu $v0, $zero, 0xa
 syscall
-
