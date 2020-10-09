3885ccd866dcdff06f73bb0f851598645608e8ad
MWalden13 <70349366+MWalden13@users.noreply.github.com>
2020-10-08 19:08:54 -0500

    Update bubble.asm

--- bubble.asm
+++ bubble.asm
@@ -20,7 +20,8 @@ sw $t7, 1c($a0)
 sw $t8, 20($a0)
 sw $t9, 24($a0) 
 sub $s0, $t1, $t0 
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t1, 0($a0)
 sw $t0, 4($a0)
 addiu $a1, $t0, 0
@@ -29,7 +30,8 @@ addiu $t0, $a2, 0
 addiu $t1, $a1, 0
 jal 0x100015
 sub $s0, $t2, $t0
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t2, 0($a0)
 sw $t0, 8($a0)
 addiu $a1, $t0, 0
@@ -38,7 +40,8 @@ addiu $t0, $a2, 0
 addiu $t2, $a1, 0
 jal 0x100015
 sub $s0, $t3, $t0
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t3, 0($a0)
 sw $t0, c($a0)
 addiu $a1, $t0, 0
@@ -47,7 +50,8 @@ addiu $t0, $a2, 0
 addiu $t3, $a1, 0
 jal 0x100015
 sub $s0, $t4, $t0
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t4, 0($a0)
 sw $t0, 10($a0)
 addiu $a1, $t0, 0
@@ -56,7 +60,8 @@ addiu $t0, $a2, 0
 addiu $t4, $a1, 0
 jal 0x100015
 sub $s0, $t5, $t0
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t5, 0($a0)
 sw $t0, 14($a0)
 addiu $a1, $t0, 0
@@ -65,7 +70,8 @@ addiu $t0, $a2, 0
 addiu $t5, $a1, 0
 jal 0x100015
 sub $s0, $t6, $t0
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t6, 0($a0)
 sw $t0, 18($a0)
 addiu $a1, $t0, 0
@@ -74,7 +80,8 @@ addiu $t0, $a2, 0
 addiu $t6, $a1, 0
 jal 0x100015
 sub $s0, $t7, $t0
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t7, 0($a0)
 sw $t0, 1c($a0)
 addiu $a1, $t0, 0
@@ -83,7 +90,8 @@ addiu $t0, $a2, 0
 addiu $t7, $a1, 0
 jal 0x100015
 sub $s0, $t8, $t0
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t8, 0($a0)
 sw $t0, 20($a0)
 addiu $a1, $t0, 0
@@ -92,7 +100,8 @@ addiu $t0, $a2, 0
 addiu $t8, $a1, 0
 jal 0x100015
 sub $s0, $t9, $t0
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t9, 0($a0)
 sw $t0, 24($a0)
 addiu $a1, $t0, 0
@@ -101,7 +110,8 @@ addiu $t0, $a2, 0
 addiu $t9, $a1, 0
 jal 0x100015
 sub $s0, $t2, $t1
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t2, 4($a0) 
 sw $t1, 8($a0)
 addiu $a1, $t1, 0
@@ -110,7 +120,8 @@ addiu $t1, $a2, 0
 addiu $t2, $a1, 0
 jal 0x100015
 sub $s0, $t3, $t1
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t3, 4($a0) 
 sw $t1, c($a0)
 addiu $a1, $t1, 0
@@ -119,7 +130,8 @@ addiu $t1, $a2, 0
 addiu $t3, $a1, 0
 jal 0x100015
 sub $s0, $t4, $t1
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t4, 4($a0) 
 sw $t1, 10($a0) 
 addiu $a1, $t1, 0
@@ -128,7 +140,8 @@ addiu $t1, $a2, 0
 addiu $t4, $a1, 0
 jal 0x100015
 sub $s0, $t5, $t1
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t5, 4($a0) 
 sw $t1, 14($a0) 
 addiu $a1, $t1, 0
@@ -137,7 +150,8 @@ addiu $t1, $a2, 0
 addiu $t5, $a1, 0
 jal 0x100015
 sub $s0, $t6, $t1
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t6, 4($a0) 
 sw $t1, 18($a0) 
 addiu $a1, $t1, 0
@@ -146,7 +160,8 @@ addiu $t1, $a2, 0
 addiu $t6, $a1, 0
 jal 0x100015
 sub $s0, $t7, $t1
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t7, 4($a0) 
 sw $t1, 1c($a0)
 addiu $a1, $t1, 0
@@ -155,7 +170,8 @@ addiu $t1, $a2, 0
 addiu $t7, $a1, 0
 jal 0x100015
 sub $s0, $t8, $t1
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t8, 4($a0) 
 sw $t1, 20($a0)
 addiu $a1, $t1, 0
@@ -164,7 +180,8 @@ addiu $t1, $a2, 0
 addiu $t8, $a1, 0
 jal 0x100015
 sub $s0, $t9, $t1
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t9, 4($a0) 
 sw $t1, 24($a0) 
 addiu $a1, $t1, 0
@@ -173,7 +190,8 @@ addiu $t1, $a2, 0
 addiu $t9, $a1, 0
 jal 0x100015
 sub $s0, $t3, $t2
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t3, 8($a0)
 sw $t2, c($a0)
 addiu $a1, $t2, 0
@@ -182,7 +200,8 @@ addiu $t2, $a2, 0
 addiu $t3, $a1, 0
 jal 0x100015
 sub $s0, $t4, $t2
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t4, 8($a0)
 sw $t2, 10($a0)
 addiu $a1, $t2, 0
@@ -191,7 +210,8 @@ addiu $t2, $a2, 0
 addiu $t4, $a1, 0
 jal 0x100015
 sub $s0, $t5, $t2
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t5, 8($a0)
 sw $t2, 14($a0)
 addiu $a1, $t2, 0
@@ -200,7 +220,8 @@ addiu $t2, $a2, 0
 addiu $t5, $a1, 0
 jal 0x100015
 sub $s0, $t6, $t2
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t6, 8($a0)
 sw $t2, 18($a0)
 addiu $a1, $t2, 0
@@ -209,7 +230,8 @@ addiu $t2, $a2, 0
 addiu $t6, $a1, 0
 jal 0x100015
 sub $s0, $t7, $t2
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t7, 8($a0)
 sw $t2, 1c($a0)
 addiu $a1, $t2, 0
@@ -218,7 +240,8 @@ addiu $t2, $a2, 0
 addiu $t7, $a1, 0
 jal 0x100015
 sub $s0, $t8, $t2
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t8, 8($a0)
 sw $t2, 20($a0)
 addiu $a1, $t2, 0
@@ -227,7 +250,8 @@ addiu $t2, $a2, 0
 addiu $t8, $a1, 0
 jal 0x100015
 sub $s0, $t9, $t2
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t9, 8($a0)
 sw $t2, 24($a0)
 addiu $a1, $t2, 0
@@ -236,7 +260,8 @@ addiu $t2, $a2, 0
 addiu $t9, $a1, 0
 jal 0x100015
 sub $s0, $t4, $t3
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t4, c($a0)
 sw $t3, 10($a0)
 addiu $a1, $t3, 0
@@ -245,7 +270,8 @@ addiu $t3, $a2, 0
 addiu $t4, $a1, 0
 jal 0x100015
 sub $s0, $t5, $t3
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t5, c($a0)
 sw $t3, 14($a0)
 addiu $a1, $t3, 0
@@ -254,7 +280,8 @@ addiu $t3, $a2, 0
 addiu $t5, $a1, 0
 jal 0x100015
 sub $s0, $t6, $t3
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t6, c($a0)
 sw $t3, 18($a0)
 addiu $a1, $t3, 0
@@ -263,7 +290,8 @@ addiu $t3, $a2, 0
 addiu $t6, $a1, 0
 jal 0x100015
 sub $s0, $t7, $t3
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t7, c($a0)
 sw $t3, 1c($a0)
 addiu $a1, $t3, 0
@@ -272,7 +300,8 @@ addiu $t3, $a2, 0
 addiu $t7, $a1, 0
 jal 0x100015
 sub $s0, $t8, $t3
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t8, c($a0)
 sw $t3, 20($a0)
 addiu $a1, $t3, 0
@@ -281,7 +310,8 @@ addiu $t3, $a2, 0
 addiu $t8, $a1, 0
 jal 0x100015
 sub $s0, $t9, $t3
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t4, c($a0)
 sw $t3, 24($a0)
 addiu $a1, $t3, 0
@@ -290,7 +320,8 @@ addiu $t3, $a2, 0
 addiu $t9, $a1, 0
 jal 0x100015
 sub $s0, $t5, $t4
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t5, 10($a0)
 sw $t4, 14($a0)
 addiu $a1, $t4, 0
@@ -299,7 +330,8 @@ addiu $t4, $a2, 0
 addiu $t5, $a1, 0
 jal 0x100015
 sub $s0, $t6, $t4
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t6, 10($a0)
 sw $t4, 18($a0)
 addiu $a1, $t4, 0
@@ -308,7 +340,8 @@ addiu $t4, $a2, 0
 addiu $t6, $a1, 0
 jal 0x100015
 sub $s0, $t7, $t4
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t7, 10($a0)
 sw $t4, 1c($a0)
 addiu $a1, $t4, 0
@@ -317,7 +350,8 @@ addiu $t4, $a2, 0
 addiu $t7, $a1, 0
 jal 0x100015
 sub $s0, $t8, $t4
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t8, 10($a0)
 sw $t4, 20($a0)
 addiu $a1, $t4, 0
@@ -326,7 +360,8 @@ addiu $t4, $a2, 0
 addiu $t8, $a1, 0
 jal 0x100015
 sub $s0, $t9, $t4
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t9, 10($a0)
 sw $t4, 24($a0)
 addiu $a1, $t4, 0
@@ -335,7 +370,8 @@ addiu $t4, $a2, 0
 addiu $t9, $a1, 0
 jal 0x100015
 sub $s0, $t6, $t5
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t6, 14($a0)
 sw $t5, 18($a0)
 addiu $a1, $t5, 0
@@ -344,7 +380,8 @@ addiu $t5, $a2, 0
 addiu $t6, $a1, 0
 jal 0x100015
 sub $s0, $t7, $t5
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t7, 14($a0)
 sw $t5, 1c($a0)
 addiu $a1, $t5, 0
@@ -353,7 +390,8 @@ addiu $t5, $a2, 0
 addiu $t7, $a1, 0
 jal 0x100015
 sub $s0, $t8, $t5
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t8, 14($a0)
 sw $t5, 20($a0)
 addiu $a1, $t5, 0
@@ -362,7 +400,8 @@ addiu $t5, $a2, 0
 addiu $t8, $a1, 0
 jal 0x100015
 sub $s0, $t9, $t5
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t9, 14($a0)
 sw $t5, 24($a0)
 addiu $a1, $t5, 0
@@ -371,7 +410,8 @@ addiu $t5, $a2, 0
 addiu $t9, $a1, 0
 jal 0x100015
 sub $s0, $t7, $t6
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t7, 18($a0)
 sw $t6, 1c($a0)
 addiu $a1, $t6, 0
@@ -380,7 +420,8 @@ addiu $t6, $a2, 0
 addiu $t7, $a1, 0
 jal 0x100015
 sub $s0, $t8, $t6
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t8, 18($a0)
 sw $t6, 20($a0)
 addiu $a1, $t6, 0
@@ -389,7 +430,8 @@ addiu $t6, $a2, 0
 addiu $t8, $a1, 0
 jal 0x100015
 sub $s0, $t9, $t6
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t9, 18($a0)
 sw $t6, 24($a0)
 addiu $a1, $t6, 0
@@ -398,7 +440,8 @@ addiu $t6, $a2, 0
 addiu $t9, $a1, 0
 jal 0x100015
 sub $s0, $t8, $t7
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t8, 1c($a0)
 sw $t7, 20($a0)
 addiu $a1, $t7, 0
@@ -407,7 +450,8 @@ addiu $t7, $a2, 0
 addiu $t8, $a1, 0
 jal 0x100015
 sub $s0, $t9, $t7
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t9, 1c($a0)
 sw $t7, 24($a0)
 addiu $a1, $t7, 0
@@ -416,7 +460,8 @@ addiu $t7, $a2, 0
 addiu $t9, $a1, 0
 jal 0x100015
 sub $s0, $t9, $t8
-bltz $s0, 1
+bltz $s0, 2
+bgtz $s0, 8
 sw $t9, 20($a0)
 sw $t8, 24($a0)
 addiu $a1, $t8, 0
